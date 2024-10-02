eaa64bc34b8106923f2b6a477bd700d6572ac075
Romanjks123 <m7playyt@gmail.com>
2024-10-02 16:16:20 +0300

    first

---
+++ 5.02.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+
+void S(const double x, const double eps, int& n, double& s);
+void A(const double x, const int n, double& a);
+
+int main()
+{
+    double xp, xk, x, dx, eps, s = 0;
+    int n = 1;
+    cout << "xp = "; cin >> xp;
+    cout << "xk = "; cin >> xk;
+    cout << "dx = "; cin >> dx;
+    cout << "eps = "; cin >> eps;
+    cout << fixed;
+    cout << "-------------------------------------------------" << endl;
+    cout << "|" << setw(7) << "x" << " |"
+        << setw(8) << "exp(x)" << " |"
+        << setw(10) << "S" << " |"
+        << setw(5) << "n" << " |"
+        << endl;
+    cout << "-------------------------------------------------" << endl;
+
+    for (x = xp; x <= xk; x += dx)
+    {
+        S(x, eps, n, s);
+        cout << "|" << setw(7) << setprecision(2) << x << " |"
+            << setw(8) << setprecision(5) << exp(x) << " |"
+            << setw(10) << setprecision(5) << s << " |"
+            << setw(5) << n << " |"
+            << endl;
+    }
+    cout << "-------------------------------------------------" << endl;
+    return 0;
+}
+
+void S(const double x, const double eps, int& n, double& s)
+{
+    n = 1;
+    double a = 1.0; // початковий член ряду
+    s = a;
+
+    do {
+        n++;
+        A(x, n, a);
+        s += a;
+    } while (abs(a) >= eps);
+}
+
+void A(const double x, const int n, double& a)
+{
+    double R = x / n; // відношення для наступного члена ряду
+    a *= R;
+}
